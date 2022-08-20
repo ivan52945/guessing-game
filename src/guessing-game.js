001d69f2e606344ca78c386ec2e373278968e827
ivan52945 <ivan52945@gmail.com>
2022-08-20 19:34:52 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+	}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
+	guess() {
+		return Math.round((this.min + this.max) / 2);
+	}
 
-    }
+	lower() {
+		this.max -= Math.floor((this.max - this.min) / 2);
+	}
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min += Math.ceil((this.max - this.min) / 2);
+	}
 }
 
 module.exports = GuessingGame;
